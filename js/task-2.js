72e1cfa4dda9092e4affb700cad250110fe2fe25
SobDim <101928077+SobDim@users.noreply.github.com>
2024-02-08 21:37:09 +0200

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -5,25 +5,23 @@
 */
 
 function makeArray(firstArray, secondArray, maxLength) {
-// перевірка на масив
-Array.isArray(firstArray) || Array.isArray(secondArray) !== true ? "Please, enter array!" : continue;
+  if (!Array.isArray(firstArray) || !Array.isArray(secondArray)) {
+    return 'Please, enter array!';
+  }
+  if (!Number.isInteger(maxLength)) {
+    return 'Please, enter number!';
+  }
+  const newArray = firstArray.concat(secondArray);
 
-перевірка на число
-
-const newArray = firstArray.concat(secondArray);
-
-return newArray;
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
+  }
+  return newArray;
 }
-// makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea']);
 
-// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea']));
-
-// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
-
-const a = ['c', 'z', 'c'];
-console.log(Array.isArray(a));
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,25 @@
 /* 
 Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри. Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
 
-
-
 Усередині функції:
-
 Створи порожній масив, у який будеш додавати підходящі числа.
 Використай цикл для ітерації кожного елемента масиву numbers.
 Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
 Поверни свій новий масив з підходящими числами як результат.
 
 */
+
+function filterArray(numbers, value) {
+  if (!Array.isArray(numbers) || !Number.isInteger(value)) {
+    return 'Please ENTER valid parameters- [Array of numbers], (integer)';
+  }
+
+  const newArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) newArray.push(numbers[i]);
+  }
+  return newArray;
+}
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
